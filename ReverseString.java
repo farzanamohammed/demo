ec01b7161273c1ca7c957c4253760afe1f510df0
farzanamohammed <farzjana@gmail.com>
2022-10-21 16:00:45 +0530

    first commit

---
+++ ReverseString.java
@@ -0,0 +1,16 @@
+package com.important_programs;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		
+		String str = "Hello";
+		char[] chArr = str.toCharArray();
+		
+		for(int i=chArr.length-1;i>=0;i--)
+		{
+			System.out.println(chArr[i]);
+		}
+	}
+
+}
